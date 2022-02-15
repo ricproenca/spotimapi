7028555966d8b27352f2ce2cc6187feb028d9e48
Ricardo Proença <ricproenca@gmail.com>
2022-02-15 19:02:24 +0100

    add mongo Db connection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import dotenv from 'dotenv';
+
 import app from './server/apiServer';
 
-app.listen(process.env.PORT || 5000, () => console.log('.. and the magic happens at port 5000!'));
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+// eslint-disable-next-line no-console
+const callback = () => console.log(`API Server on port ${port}!`);
+
+app.listen(port, callback);
