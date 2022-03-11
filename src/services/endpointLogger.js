561b38720c2895cc1d020cb68d4811509a005bac
Ricardo Proença <ricproenca@gmail.com>
2022-03-11 23:28:52 +0100

    - fix logger

--- src/services/endpointLogger.js
+++ src/services/endpointLogger.js
@@ -1,16 +1,18 @@
 import logger from './logger';
 
 const endpointLogger = (req, res, next) => {
-  logger.info(`Request Method: ${req.method}`);
-  logger.info(`Request URL: ${req.url}`);
-  logger.info(`Request Body: ${JSON.stringify(req.body)}`);
-  logger.info(`Request Headers: ${JSON.stringify(req.headers)}`);
+  logger.info('=== REQUEST ===');
+  logger.info(`Method: ${req.method}`);
+  logger.info(`URL: ${req.url}`);
+  logger.info(`Body: ${JSON.stringify(req.body)}`);
+  logger.info(`Headers: ${JSON.stringify(req.headers)}`);
 
   res.on('finish', () => {
-    logger.info(`Response Code: ${res.statusCode}`);
-    logger.info(`Response Message: ${res.statusMessage}`);
-    logger.info(`Response Body : ${JSON.stringify(res.body)}`);
-    logger.info(`Response Headers: ${JSON.stringify(res.getHeaders())}`);
+    logger.info('=== RESPONSE ===');
+    logger.info(`Code: ${res.statusCode}`);
+    logger.info(`Message: ${res.statusMessage}`);
+    logger.info(`Body : ${JSON.stringify(res.body)}`);
+    logger.info(`Headers: ${JSON.stringify(res.getHeaders())}`);
   });
 
   next();
